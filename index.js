ba17c67aeec73a669ad37692895048365433d497
shuhei.morioka <selmertsx@gmail.com>
2018-10-04 16:09:58 +0900

    index.jsをコードがbuildされたら、その結果をslackに通知するfunctionに書き直した

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const WebClient = require("@slack/client").WebClient;
-const slackToken = "";
+const slackToken = process.env.SLACK_TOKEN;
 const client = new WebClient(slackToken);
-const channelID = "";
+const channelID = process.env.CHANNEL_ID;
 
-module.exports.subscribe = (event, callback)=>{
-  console.log(event);
-  console.log("test");
-  return callback(null, "OK");
+module.exports.subscribe = (event, callback)=> {
+  /*
+  const build = eventToBuild(event.data.data);
+  const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
+
+  if (status.indexOf(build.status) === -1) {
+    return callback();
+  }
+
+  await client.chat.postMessage({
+    channel: channelID,
+    text: `Build \`${build.id}\``,
+    attachments: [{
+      title: "Build Logs",
+      title_link: build.logUrl,
+      fields: [{
+        title: 'Status',
+        value: build.status
+      }]
+    }]
+  });
+  */
+  return callback(null, "Success");
+}
+
+const eventToBuild = (data) => {
+  return JSON.parse(new Buffer(data, 'base64').toString());
 }
