e7ba9258f152628cb0414765d8c644c81a80d9c6
shuhei.morioka <selmertsx@gmail.com>
2018-10-04 20:02:56 +0900

    encrypt .env.json & keyfile.json

--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -1,1 +1,2 @@
 gcloud kms encrypt --plaintext-file .env.json --ciphertext-file .env.json.enc --location global --keyring sample-keyrings --key sample-key
+gcloud kms encrypt --plaintext-file keyfile.json --ciphertext-file keyfile.json.enc --location global --keyring sample-keyrings --key sample-key
