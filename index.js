b0cf3071ce6a5de113fbe8bacfd17dd6cc1b39d4
shuhei.morioka <selmertsx@gmail.com>
2018-10-04 14:28:58 +0900

    シンプルなCloud FunctionsをDeployしてみる

--- index.js
+++ index.js
@@ -4,27 +4,6 @@ const client = new WebClient(slackToken);
 const channelID = "";
 
 module.exports.subscribe = (event, callback)=>{
-  const build = eventToBuild(event.data.data);
-  const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
-
-  if (status.indexOf(build.status) === -1) {
-    return callback();
-  }
-
-  client.chat.postMessage({
-    channel: channelID,
-    text: `Build \`${build.id}\``,
-    attachments: [{
-      title: "Build Logs",
-      title_link: build.logUrl,
-      fields: [{
-        title: 'Status',
-        value: build.status
-      }]
-    }]
-  });
-}
-
-const eventToBuild = (data) => {
-  return JSON.parse(new Buffer(data, 'base64').toString());
+  console.log(event);
+  console.log("test");
 }
