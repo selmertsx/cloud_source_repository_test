313956c5c7a5d08fecee91626dc76dc4a3e811c3
shuhei.morioka <selmertsx@gmail.com>
2018-10-04 18:52:06 +0900

    serverless frameworkに合わせて今のコードを書き直した

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-const WebClient = require("@slack/client").WebClient;
-const slackToken = process.env.SLACK_TOKEN;
-const client = new WebClient(slackToken);
-const channelID = process.env.CHANNEL_ID;
-
-module.exports.subscribe = function subscribe(event, callback) {
-  /*
-  const build = eventToBuild(event.data.data);
-  const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
-
-  if (status.indexOf(build.status) === -1) {
-    return callback();
-  }
-
-  await client.chat.postMessage({
-    channel: channelID,
-    text: `Build \`${build.id}\``,
-    attachments: [{
-      title: "Build Logs",
-      title_link: build.logUrl,
-      fields: [{
-        title: 'Status',
-        value: build.status
-      }]
-    }]
-  });
-  */
-
-  console.log(process.env.SLACK_TOKEN);
+exports.subscribe = (event, callback) => {
   return callback(null, "Success");
-}
-
-const eventToBuild = (data) => {
-  return JSON.parse(new Buffer(data, 'base64').toString());
 }
