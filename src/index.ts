27c84694928bd937ef679672b9f32f600fab1ed0
shuhei.morioka <selmertsx@gmail.com>
2018-10-05 20:02:40 +0900

    returnを取り除いてみた

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { Decimal } from "decimal.js";
 
-function subscribe(event: any, callback: any) {
+exports.subscribe = (event: any, callback: any) => {
   const x = new Decimal(0.3).minus(0.1);
-  return callback(null, x);
-}
-
-export { subscribe };
+  callback(null, x);
+};
