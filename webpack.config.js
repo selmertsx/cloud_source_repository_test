42762586d24d2a6f87937c380780ac866c8edfc5
shuhei.morioka <selmertsx@gmail.com>
2018-10-05 19:01:32 +0900

    npm i -D ts-loader webpack webpack-cli

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, '.build'),
+    library: 'index',
+    libraryTarget: 'commonjs2'
+  }
+};
