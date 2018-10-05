ef47f825127064cce82caddb61e3eacbe80d2f2f
shuhei.morioka <selmertsx@gmail.com>
2018-10-05 18:45:55 +0900

    test import decimal package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { Decimal } from "decimal.js";
+
 function subscribe(event: any, callback: any) {
-  return callback(null, "Success");
+  const x = new Decimal(0.3).minus(0.1);
+  return callback(null, x);
 }
 
 export { subscribe };
