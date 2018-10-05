785072694cb46b173a67d0589173112ae0c3b414
shuhei.morioka <selmertsx@gmail.com>
2018-10-05 11:10:37 +0900

    create file.json

--- bin/encrypt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eu
-
-gcloud kms encrypt --plaintext-file .env.json --ciphertext-file .env.json.enc --location global --keyring sample-keyrings --key sample-key
-gcloud kms encrypt --plaintext-file keyfile.json --ciphertext-file keyfile.json.enc --location global --keyring sample-keyrings --key sample-key
