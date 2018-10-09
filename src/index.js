868e981a852646e2a6ef9b3fa688fe2719f924be
shuhei.morioka <selmertsx@gmail.com>
2018-10-09 16:28:31 +0900

    外部moduleを読み込んで実行できることが確認できた。なぜか constしないと駄目だった。

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-//import * as Decimal from "decimal.js";
+//import {Decimal} from "decimal.js"; // これだと駄目
 
+const Decimal = require('decimal.js'); //これだと動く
 module.exports.subscribe = (req, res) => {
-  return res.send(`Hello World!`);
+  const x = new Decimal('0xff.f')
+  return res.send(`Hello World! ${x}`);
 }
