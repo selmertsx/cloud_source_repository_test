0dd537ac40707cc44aecc73813426c469c09c2ef
shuhei.morioka <selmertsx@gmail.com>
2018-10-09 16:09:27 +0900

    import Decimal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { Decimal } from "decimal.js";
+
 module.exports.subscribe = (req, res) => {
-  return res.send("Hello World!");
+  const x = new Decimal(0.3).minus(0.1);
+  return res.send(`Hello World! ${x}`);
 }
